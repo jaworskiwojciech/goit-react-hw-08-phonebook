32802a43fcf70f857fcc76e97b4837ab0162723d
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-07-21 22:30:10 +0200

    styled

---
+++ src/components/AuthNav/AuthNav.styled.jsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+import { NavLink } from 'react-router-dom';
+
+export const Link = styled(NavLink)`
+  margin: 6px 5px;
+  text-decoration: none;
+  color: #000000;
+  font-size: 14px;
+  font-weight: 500;
+  font-family: ubuntu;
+  @media (min-width: 600px) {
+    font-size: 18px;
+    margin: 8px 10px;
+  }
+
+  &.active {
+    color: #f5f4fa;
+  }
+  transition: transform var(--transition);
+  &:hover,
+  &:focus {
+    transform: scale(1.07);
+  }
+`;
