bf7bc47b9227725f65ddfd62629087d582d16b78
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-07-21 23:04:13 +0200

    User Menu

---
+++ src/components/UserMenu/UserMenu.jsx
@@ -0,0 +1,49 @@
+import { useAuth } from 'hooks';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout } from 'redux/auth/operations';
+import { User } from './UserMenu.styled';
+import { Button } from '@mui/material';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import { styled } from '@mui/material/styles';
+import { selectIsAuthLoading } from 'redux/auth/selectors';
+import { LogoutLoader } from 'components/Loader';
+
+const LogoutButton = styled(Button)`
+  background-color: #ffffff;
+  color: #1976d2;
+  border: 1px solid #ffffff;
+  font-size: 10px;
+  padding: 5px 6px;
+
+  @media (min-width: 600px) {
+    font-size: 12px;
+    padding: 6px 8px;
+  }
+
+  :hover {
+    color: #ffffff;
+  }
+`;
+
+export const UserMenu = () => {
+  const { user } = useAuth();
+  const dispatch = useDispatch();
+  const authOperation = useSelector(selectIsAuthLoading);
+
+  // console.log('authOperation', authOperation)
+
+  return (
+    <>
+      <User>{user.name}</User>
+      <LogoutButton
+        type="button"
+        onClick={() => dispatch(logout())}
+        endIcon={
+          authOperation === 'logout' ? <LogoutLoader /> : <ExitToAppIcon />
+        }
+      >
+        Logout
+      </LogoutButton>
+    </>
+  );
+};
---
+++ src/components/UserMenu/UserMenu.styled.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+export const User = styled.p`
+  margin-left: auto;
+  margin-right: 5px;
+  color: #f5f4fa;
+  font-size: 14px;
+  font-family: ubuntu;
+
+  @media (min-width: 600px) {
+    font-size: 16px;
+    margin-right: 20px;
+  }
+`;
