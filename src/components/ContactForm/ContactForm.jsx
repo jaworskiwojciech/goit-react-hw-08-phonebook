5ddafc681bde196b7db1b74806cc9836c84b2022
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-07-21 22:47:18 +0200

    contact form

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,90 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { selectContacts, selectIsLoading } from 'redux/contacts/selectors';
+import { addContact } from 'redux/contacts/operations';
+import { toast } from 'react-hot-toast';
+import { Box, Button, Container, TextField } from '@mui/material';
+import { AddLoader } from 'components/Loader';
+
+export const ContactForm = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const operation = useSelector(selectIsLoading);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const name = form.elements.name.value;
+    const number = form.elements.number.value;
+
+    const newName = contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+    if (newName) {
+      toast.error(`${name} is already in contacts`);
+      return;
+    }
+    if (name === '' || number === '') {
+      toast.error('Fields cannot be empty. Enter some data!');
+      return;
+    }
+    try {
+      await dispatch(addContact({ name: name, number: number })).unwrap();
+      toast.success(`${name} is added to contacts`);
+      form.reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Container
+      maxWidth="xs"
+      sx={{
+        p: 4,
+        mb: 4,
+        bgcolor: '#ffffff',
+        borderRadius: '10px',
+        boxShadow: 3,
+      }}
+    >
+      <Box
+        component="form"
+        autoComplete="off"
+        noValidate
+        onSubmit={handleSubmit}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="name"
+          label="Name"
+          name="name"
+          size="small"
+        />
+
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          type="tel"
+          id="number"
+          label="Phone Number"
+          name="number"
+          size="small"
+        />
+
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          sx={{ mt: 2, mb: 2 }}
+          size="medium"
+        >
+          {operation === 'add' ? <AddLoader /> : <>Add contact</>}
+        </Button>
+      </Box>
+    </Container>
+  );
+};
