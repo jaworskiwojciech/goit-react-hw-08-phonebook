d348a4100af31aeafd8af463a33b2449a51ed4b2
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-07-21 22:31:25 +0200

    AuthNav

---
+++ src/components/AuthNav/AuthNav.jsx
@@ -0,0 +1,10 @@
+import { Link } from './AuthNav.styled';
+
+export const AuthNav = () => {
+  return (
+    <>
+      <Link to="/login">Login</Link>
+      <Link to="/register">Register</Link>
+    </>
+  );
+};
