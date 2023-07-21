1f9d49f4e43dacaad877b6aeba4a9a3868d1c6f7
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-07-21 22:57:18 +0200

    Layout

---
+++ src/components/Layout/Layout.jsx
@@ -0,0 +1,21 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { Bar } from 'components/AppBar/AppBar';
+import { Container } from '@mui/material';
+import { Footer } from 'components/Footer/Footer';
+
+const Layout = () => {
+  return (
+    <Container maxWidth="md">
+      <Bar />
+      <Suspense fallback={null}>
+        <Outlet />
+      </Suspense>
+      <Footer />
+      <Toaster position="top-right" reverseOrder={false} />
+    </Container>
+  );
+};
+
+export default Layout;
