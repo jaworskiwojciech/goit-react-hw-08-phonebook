a098acbd4dbc33386b6360f600f0fb3337bc383b
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-07-21 22:50:41 +0200

    Error Page

---
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Image } from './ErrorPage.styled';
+import imgError from '../../images/page-404.png';
+
+const ErrorPage = () => {
+  return <Image src={imgError} alt={'Page 404'} />;
+};
+
+export default ErrorPage;
