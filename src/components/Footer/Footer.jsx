fad084c5549e5c9f90fcd5e375132900a2df9a0a
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-07-21 22:55:34 +0200

    Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import { Box, Link, Typography } from '@mui/material';
+
+function Copyright(props) {
+  return (
+    <Box color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Typography variant="body2" color="text.secondary">
+        Developed by{' '}
+        <Link
+          color="inherit"
+          target="_blank"
+          rel="noopener noreferrer"
+          href="https://github.com/Samokhvalova-Nata"
+        >
+          Samokhvalova Nataliia
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
+    </Box>
+  );
+}
+
+export const Footer = () => {
+  return (
+    <Box component="footer">
+      <Box
+        sx={{
+          // marginTop: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          bgcolor: 'rgba(0, 0, 0, 0.07 )',
+        }}
+      >
+        <Copyright sx={{ my: 1.5 }} />
+      </Box>
+    </Box>
+  );
+};
