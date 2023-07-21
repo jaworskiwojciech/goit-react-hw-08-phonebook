c865bb39bd0c490969a49fc7a37f79401b4fb56d
jaworskiwojciech <jaworski.wojciech94@gmail.com>
2023-07-21 22:49:33 +0200

    Contact List

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,26 @@
+import { useSelector } from 'react-redux';
+import { ContactItem } from '../ContactItem/ContactItem';
+import { selectVisibleContacts } from 'redux/contacts/selectors';
+import { Container } from '@mui/material';
+
+export const ContactList = () => {
+  const visibleContacts = useSelector(selectVisibleContacts);
+
+  return (
+    <Container
+      maxWidth="md"
+      sx={{
+        p: 4,
+        mt: 4,
+        bgcolor: '#ffffff',
+        borderRadius: '10px',
+        boxShadow: 3,
+      }}
+      component="ul"
+    >
+      {visibleContacts.map(({ id, name, number }) => (
+        <ContactItem key={id} id={id} name={name} number={number} />
+      ))}
+    </Container>
+  );
+};
